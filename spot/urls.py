6e2218178fb398aca890c06ae5f860b78b4e6821
rbs2rbs <renan.bisposilva@gmail.com>
2020-05-04 10:18:28 -0300

    Ajusta cach

--- spot/urls.py
+++ spot/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from .views import index
 
+from django.views.decorators.cache import never_cache
+
 router = routers.DefaultRouter()
 # router.register(r'pagina',views.PaginaView,'pagina')  
 
@@ -14,7 +16,7 @@ urlpatterns = [
     path('api/musica/',views.MusicaView.as_view()),
     path('api/comp/',views.CompView.as_view()),
     path('api/top/',views.TopView.as_view()),
-    url(r'^(?!admin|api|media).*$', index),
+    url(r'^(?!admin|api|media).*$', never_cache(index)),
     path('admin/', admin.site.urls),
     # path('api/', include(router.urls)),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- spot/views.py
+++ spot/views.py
@@ -2,4 +2,14 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 from django.contrib import admin
 
-index = never_cache(TemplateView.as_view(template_name='index.html'))
+# index = never_cache(TemplateView.as_view(template_name='index.html'))
+
+
+def index(request):
+    for key in list(request.session.keys()):
+        try:
+            del request.session[key]
+        except:
+            pass
+
+    return TemplateView.as_view(template_name='index.html')(request)
