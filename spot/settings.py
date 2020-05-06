a2dd22f4be2419594673ebb65ad62bd0b52d40dc
rbs2rbs <renan.bisposilva@gmail.com>
2020-05-06 12:20:58 -0300

    Ajusta banco de dados

--- spot/settings.py
+++ spot/settings.py
@@ -89,7 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'OPTIONS': {
+        'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
+    },
 }
 
 
